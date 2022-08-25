ba400d85c60db57b9ca4f164670d8c821ce450bd
MostafaHeidari <70130828+MostafaHeidari@users.noreply.github.com>
2022-08-25 11:24:04 +0200

    // den nye router for at hente data.

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -29,5 +29,31 @@ namespace WebApplication1.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> GGet()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
+
+        // den nye router for at hente data.
+        [HttpGet(Name = "GetWeatherForecast")]
+        [Route("g")]
+        public IEnumerable<WeatherForecast> GGGet()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
     }
 }
